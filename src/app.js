90ef77cfde54f18cdd989ad2b28692e11e3bce86
mynameiskatchy <karchietrinh@gmail.com>
2020-03-15 13:53:08 -0400

    Get root reactDOM componenet to render
    Untracked files:
    	package-lock.json
        public/bundle.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Main from './components/main.jsx';
+
+// ReactDOM.render(<Main />, document.getElementById('app'));
+// ReactDOM.render(
+//     <Main />,
+//     document.getElementById('app')
+// );
+
+
+ReactDOM.render(<div>Hi from root react compoenent</div>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,71 @@
-var webpack = require('webpack');
+var webpack = require('webpack'); 
 
 var path = require("path");
-var srcPath = path.resolve(__dirname, "client");
-var distPath = path.resolve(__dirname, "public");
 
-var config = {
-    devtool: 'source-map',
-    entry: [
-        srcPath + "/app.js"
-    ],
+module.exports = {
+    entry: './src/app.js',
     output: {
-        path: distPath,
-        publicPath: '/',
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                include: /client/,
-                loader: "babel-loader",
-                query: {
-                    presets: ['es2015']
-                }
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                include: /client/,
-                loader: "babel-loader",
-                query: {
-                    presets: ['es2015', 'stage-2', 'react']
-                }
-            }
-        ]
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }
+    ]
     },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
+    },
+    mode: 'development',
     devServer: {
-        hot: true,
-        port: 4500
+        contentBase: path.resolve(__dirname, 'public')
     }
-}
 
-module.exports = config; 
+};
+
+// var config = {
+//     devtool: 'source-map',
+//     entry: [
+//         srcPath + "/app.js"
+//     ],
+//     output: {
+//         path: distPath,
+//         publicPath: '/',
+//         filename: "bundle.js"
+//     },
+//     resolve: {
+//         extensions: ['.js', '.jsx']
+//     },
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.js?$/,
+//                 exclude: /node_modules/,
+//                 include: /client/,
+//                 loader: "babel-loader",
+//                 query: {
+//                     presets: ['es2015']
+//                 }
+//             },
+//             {
+//                 test: /\.jsx?$/,
+//                 exclude: /node_modules/,
+//                 include: /client/,
+//                 loader: "babel-loader",
+//                 query: {
+//                     presets: ['es2015', 'stage-2', 'react']
+//                 }
+//             }
+//         ]
+//     },
+//     devServer: {
+//         hot: true,
+//         port: 4500
+//     }
+// }
+
+// module.exports = config; 
